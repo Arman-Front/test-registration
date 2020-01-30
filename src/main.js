d76ab3a9ced9f6ad7d7c1081b38ed484403bf60f
arman.kh <arman.kh@forbet.net>
2020-01-30 15:46:49 +0300

    connect vuex in project

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vuelidate from 'vuelidate'
 import VueRouter from 'vue-router'
 import router from './routes'
 import App from './App.vue'
+import store from './store'
 
 Vue.config.productionTip = false;
 Vue.use(Vuelidate);
@@ -10,5 +11,6 @@ Vue.use(VueRouter);
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app');
